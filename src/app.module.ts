a85f6446eccb571472fb463d16f0b6d49cdb9065
hahuyvu975 <hahuyvu975@gmail.com>
2024-03-07 13:12:30 +0700

    fix imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { NoteModule } from './note/note.module';
 
 @Module({
   imports: [
-    
-  AuthModule],
+    AuthModule,
+    UserModule,
+    NoteModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
